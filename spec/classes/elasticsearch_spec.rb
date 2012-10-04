45b34069cb8b9c28a35e80e0269e1444f924fae4
Tim Sharpe <tim@github.com>
2012-10-04 10:07:09 -0700

    specs for elasticsearch class

---
+++ spec/classes/elasticsearch_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'elasticsearch' do
+  let(:facts) do
+    {
+      :boxen_home => '/opt/boxen',
+    }
+  end
+
+  it { should include_class('elasticsearch::config') }
+  it { should include_class('java') }
+
+  it do
+    should contain_package('boxen/brews/elasticsearch').with({
+      :ensure => '0.19.9-boxen1',
+      :notify => 'Service[com.boxen.elasticsearch]',
+    })
+  end
+
+  it do
+    should contain_service('com.boxen.elasticsearch').with({
+      :ensure  => 'running',
+      :require => 'Package[boxen/brews/elasticsearch]',
+    })
+  end
+
+  it do
+    should contain_file('/opt/boxen/env.d/elasticsearch.sh').with({
+      :content => File.read('spec/fixtures/elasticsearch.sh'),
+      :require => 'File[/opt/boxen/env.d]',
+    })
+  end
+end
