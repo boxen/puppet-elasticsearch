a28c26030f8009821048eeea277c983ac9e95e24
Mark Tareshawty <tarebyte@gmail.com>
2014-07-04 19:59:49 -0400

    include_class is no longer available.

--- spec/classes/elasticsearch_spec.rb
+++ spec/classes/elasticsearch_spec.rb
@@ -4,10 +4,10 @@ describe 'elasticsearch' do
   let(:facts) { default_test_facts }
 
   it do
-    should include_class("elasticsearch::config")
-    should include_class("elasticsearch::package")
-    should include_class("elasticsearch::service")
+    should contain_class("elasticsearch::config")
+    should contain_class("elasticsearch::package")
+    should contain_class("elasticsearch::service")
 
-    should include_class("java")
+    should contain_class("java")
   end
 end
